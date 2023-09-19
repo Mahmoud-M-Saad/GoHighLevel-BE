cb1c06f03d4e3bb0358b22ae54a51be75892dc92
Welcome Sir <mahmoudsaad6675@gmail.com>
2023-09-19 15:40:39 +0300

    last try

--- getopp.js
+++ getopp.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 app.get('/crm-webhook', (req, res) => {
-        return res.redirect('https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=code&redirect_uri=http://localhost:3000&client_id=65097ea78ef2c94808317db6-lmq7ce77&scope=contact.readonly')  
+        return res.redirect('https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=code&redirect_uri=https://geekyairgohighlevel.onrender.com/auth/test&client_id=65097ea78ef2c94808317db6-lmq7ce77&scope=contact.readonly')  
     });
+    app.get('auth/test',(req ,res)=>{
+console.log(req.body);
+res.json(req.body)
+    })
     app.listen(3000, () => {
         console.log(`Server is listening on port 3000`);
     });
