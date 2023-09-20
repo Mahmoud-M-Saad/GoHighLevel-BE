0f2aa373f222dd0013d2c8c5e1c88088c9b13b1a
Welcome Sir <mahmoudsaad6675@gmail.com>
2023-09-20 15:31:56 +0300

    getting code automatically

--- getopp.js
+++ getopp.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const app = express();
-const scope = [
-    "contacts.readonly" , "contacts.write" 
-]
-app.get('/crm-webhook', (req, res) => {
-        return res.redirect(`https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=code&redirect_uri=https://geekyairgohighlevel.onrender.com/auth/test&client_id=65097ea78ef2c94808317db6-lmq7ce77&scope=${scope.join(' ')}`)  
-    });
-    app.get('auth/test',(req ,res)=>{
-console.log(req.body);
-res.json(req.body)
-    })
-    app.listen(3000, () => {
-        console.log(`Server is listening on port 3000`);
-    });
+app.get('/gettingCode', (req, res) => {
+    let code = req.query.code;
+    console.log("Getting the code Successfully, the code is:"+code);
+    res.status(200).json({msg: "Getting the code Successfully"});
+})
+app.listen(3000, () => {
+    console.log(`Server is listening on port 3000`);
+});
