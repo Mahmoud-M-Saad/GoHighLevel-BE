11df2083407bc68a68825885d6a44d877fdffe61
Welcome Sir <mahmoudsaad6675@gmail.com>
2023-09-20 15:32:46 +0300

    getting code automatically

--- getopp.js
+++ getopp.js
@@ -4,7 +4,7 @@ app.get('/gettingCode', (req, res) => {
     let code = req.query.code;
     console.log("Getting the code Successfully, the code is:"+code);
     res.status(200).json({msg: "Getting the code Successfully"});
-})
+});
 app.listen(3000, () => {
     console.log(`Server is listening on port 3000`);
 });
