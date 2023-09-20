5716934b9402ab422808a281124231aa885a2506
Welcome Sir <mahmoudsaad6675@gmail.com>
2023-09-20 14:20:52 +0300

    last try

--- getopp.js
+++ getopp.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const scope = [
+    "contacts.readonly" , "contacts.write" 
+]
 app.get('/crm-webhook', (req, res) => {
-        return res.redirect('https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=code&redirect_uri=https://geekyairgohighlevel.onrender.com/auth/test&client_id=65097ea78ef2c94808317db6-lmq7ce77&scope=contact.readonly')  
+        return res.redirect(`https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=code&redirect_uri=https://geekyairgohighlevel.onrender.com/auth/test&client_id=65097ea78ef2c94808317db6-lmq7ce77&scope=${scope.join(' ')}`)  
     });
     app.get('auth/test',(req ,res)=>{
 console.log(req.body);
