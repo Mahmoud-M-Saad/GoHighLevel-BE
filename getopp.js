7292df615f56b7cc95a2ddda5159a25663c5ba3f
Welcome Sir <mahmoudsaad6675@gmail.com>
2023-09-22 15:35:38 +0300

    testing last

--- getopp.js
+++ getopp.js
@@ -4,9 +4,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const axios = require('axios').default;
 const locationId = "8KyubGi8XhoKHCpIvzGp";
-// ---- Testing Client ---- const client_id =
-// "65097ea78ef2c94808317db6-lmt7okly"; const client_secret =
-// "4354e6ce-6dcd-4f4a-9f45-5a278177fbfe"
+// ---- Testing Client ---- 
+// const client_id = "65097ea78ef2c94808317db6-lmt7okly";
+// const client_secret = "4354e6ce-6dcd-4f4a-9f45-5a278177fbfe";
 // ------------------------
 const client_id = "650477d15e0035fbc8737c87-lmkrakx4";
 const client_secret = "92867618-14e0-4392-961d-a5fbc4502780";
@@ -189,12 +189,13 @@ app.post('/upsertContact', (req, res) => {
         default:
             stageId = "Case Name not found";
     }
-    let newdata = structuredClone(req.body);
+    let newdata = JSON.parse(JSON.stringify(req.body));
+    newdata.locationId = locationId;
     delete newdata.status;
     delete newdata.stage;
-    newdata.locationId = locationId;
     console.log("Getting the data Successfully");
     // console.log(req.body);
+    // console.log(newdata);
     fs.readFile("./DBTokens.json", (err, data) => {
         access_token = JSON
             .parse(data)
@@ -243,6 +244,7 @@ app.post('/upsertContact', (req, res) => {
                             "contactId": upsertContactReq.data.contact.id,
                             "succeded": upsertContactReq.data.succeded
                         };
+                        console.log(upsertContactReq.data.succeded);
                         fs.readFile("./DBTokens.json", (err, data) => {
                             access_token = JSON
                                 .parse(data)
